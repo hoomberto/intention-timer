8b3e31e0141bd47107502b6c706a97878c722461
Taylor Galloway <gallowaytaylor@outlook.com>
2021-04-14 20:22:49 -0500

    Add query selectors to JS

---
+++ main.js
@@ -0,0 +1,5 @@
+var currentView = document.querySelector('.current-activity');
+var timerContainer = document.querySelector('.current-activity');
+var timerNumbers = document.querySelector('.timer-numbers')
+var timerCategory = document.querySelector('#actual-timer-container p')
+var startTimerButton = document.querySelector('.start-timer')
