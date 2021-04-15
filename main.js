5078b362a6e9a71f91f8492f2bda272fa100b451
Taylor Galloway <gallowaytaylor@outlook.com>
2021-04-14 20:23:29 -0500

    Add event listener for timer and temporary current activity variable

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ var timerContainer = document.querySelector('.current-activity');
 var timerNumbers = document.querySelector('.timer-numbers')
 var timerCategory = document.querySelector('#actual-timer-container p')
 var startTimerButton = document.querySelector('.start-timer')
+
+startTimerButton.addEventListener('click', startCountDown);
+
+//get input from user form
+// input.value
+
+
+// var currentActivity = new Activity(category, description, minutes, seconds);
+
+var currentActivity = {
+  category: "Deep Breathing",
+  minutes: 10,
+  seconds: 2
+}
+
+var savedActivities;
